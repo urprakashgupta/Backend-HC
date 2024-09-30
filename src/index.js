f88df5fc11adea5c9f951cb2cbc96bf71790e039
urprakashgupta <urprakashgupta@gmail.com>
2024-09-30 16:46:45 +0530

    add utils files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 ,() => {
+        console.log(`server running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("Server running failed" , err);
+    
+});
